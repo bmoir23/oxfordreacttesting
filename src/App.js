69ee9f7f92876aca322ff4bdcb9e4989c24afa3e
Dominick Bruno <dbruno@unseen.is>
2019-08-10 02:10:31 -0400

    Basic layout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BrowserRouter as Router, Link, Route, NavLink} from 'react-router-dom';
+import objConfig from './components/objConfig.js';
+import { Image, Divider, Button, Icon, Menu } from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.min.css';
+import Logo from './img/oxfordlogo.png';
 import './App.css';
 
+
+
 function App() {
+
   return (
     <div className="App">
+      
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Image src={Logo}  alt={objConfig.logoAlt} size='small' verticalAlign='top' rounded />
+        <Divider hidden />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <Menu>
+          <Menu.Item>
+        <Button animated>
+          <Button.Content visible><NavLink exact to="/">Home</NavLink></Button.Content>
+          <Button.Content hidden><Icon color='darkcyan' name='home' /></Button.Content>
+        </Button>
+        </Menu.Item>
+        <Menu.Item>
+        <Button animated>
+          <Button.Content visible><NavLink to="/contact">Contact</NavLink></Button.Content>
+          <Button.Content hidden><Icon color='darkcyan' name='mail' /></Button.Content>
+        </Button>
+        </Menu.Item>
+        <Menu.Item>
+        <Button animated>
+          <Button.Content visible>Forms</Button.Content>
+        </Button>
+        </Menu.Item>
+        </Menu>
         </p>
         <a
           className="App-link"
@@ -19,7 +46,17 @@ function App() {
           Learn React
         </a>
       </header>
+      <Divider />
+      <content></content>
+      <footer>
+      <p>&copy; 2019 {objConfig.organization}</p>
+        <p>Design by <Link to="/https://github.com/bmoir23/"> Brian Moir</Link> and <Link to="https://github.com/DomBruno/"> Dominick Bruno </ Link>
+        </p>
+        <p>{objConfig.appName} is licensed under <Link to="https://www.gnu.org/licenses/gpl.html"><Image src="https://www.gnu.org/graphics/gplv3-88x31.png" centered /></Link></p>
+      </footer>
     </div>
+    
+    
   );
 }
 
---
+++ src/components/objConfig.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const objConfig = {
+    "appName":"Oxford Houses Form Organizer",
+    "organization":"Oxford Houses",
+    "logoAlt":"Oxford Houses - Recovery, Responsibility, Replication",
+}
+
+export default objConfig;
